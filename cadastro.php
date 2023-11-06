42a020c5f160e7d8cabcb016c0392e75a8d9005a
mesquitayz <142599900+mesquitaxz@users.noreply.github.com>
2023-11-06 13:07:42 -0400

    Add files via upload

---
+++ cadastro.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Resultado</title>
+</head>
+<body>
+    <header>
+        <h1>Resultado de Processamento</h1>
+    </header>
+    <main>
+        <?php
+        $n = $_GET ["nome"];
+        $s = $_GET ["sobrenome"];
+        ?>
+        <p><a href="javascript:history.go(-1)">Voltar para a página anterior</a></p>
+    </main> 
+</body>
+</html>
